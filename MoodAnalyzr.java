41bfbf29bdcabad2631f5d3a4b686070dfa86018
redBOY <43438363+redBOY@users.noreply.github.com>
2022-09-12 14:43:13 +0530

    initial commit

---
+++ MoodAnalyzr.java
@@ -0,0 +1,21 @@
+package bridgelabzMoodAnalyzer;
+
+public class MoodAnalyzr {
+    String message;
+    public MoodAnalyzr(String message) {
+        this.message=message;
+    }
+    public MoodAnalyzr() {
+    }
+    public String analyzeMood() throws MoodAnalyzerException {
+        try {
+            if(message.contains("sad")){
+                return "SAD";
+            } else {
+                return "HAPPY";
+            }
+        } catch(NullPointerException e) {
+            throw new MoodAnalyzerException(MoodAnalyzerException.exceptionType.NULL,("Message is Null"));
+        }
+    }
+}
